7f908453edca4d865d2f675bff2e6ca763d63583
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-02-18 16:29:44 -0500

    Added Script to Player, overrode Physics and Input methods.

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,4 +3,15 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        GD.Print("Player Physics Process");
+        // base._PhysicsProcess(delta);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print("Player Input: " + @event.ToString());
+        // base._Input(@event);
+    }
 }
