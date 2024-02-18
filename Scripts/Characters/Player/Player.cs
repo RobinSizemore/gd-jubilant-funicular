fc1049405c84bb73eb580a18627e931cb2959cc0
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-02-18 18:57:57 -0500

    Moving the Player class.

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,15 +3,19 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+
+    private Vector2 direction = new Vector2();
     public override void _PhysicsProcess(double delta)
     {
-        GD.Print("Player Physics Process");
-        // base._PhysicsProcess(delta);
+        int Speed = 5;
+        Velocity = new Vector3(direction.X, 0, direction.Y).Normalized() * Speed;
+
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
-        GD.Print("Player Input: " + @event.ToString());
+        direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
         // base._Input(@event);
     }
 }
