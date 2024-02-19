1b62b4f76c61e18cfe89508239f983ecf432b1a1
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-02-18 19:19:36 -0500

    Game Constants.

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -14,12 +14,8 @@ public partial class Player : CharacterBody3D
     public override void _Ready()
     {
 
-        animPlayerNode.Play("Idle");
+        animPlayerNode.Play(GameConstants.ANIM_IDLE);
 
-        /*
-        animPlayerNode = GetNode<AnimationPlayer>("AnimationPlayer");
-        base._Ready();
-        */
         GD.Print(animPlayerNode.Name);
         GD.Print(sprite3DNode.Name);
     }
@@ -35,14 +31,14 @@ public partial class Player : CharacterBody3D
 
     public override void _Input(InputEvent @event)
     {
-        direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        direction = Input.GetVector(GameConstants.MOVE_LEFT, GameConstants.MOVE_RIGHT, GameConstants.MOVE_FORWARD, GameConstants.MOVE_BACKWARD);
         if (direction == Vector2.Zero)
         {
-            animPlayerNode.Play("Idle");
+            animPlayerNode.Play(GameConstants.ANIM_IDLE);
         }
         else
         {
-            animPlayerNode.Play("Move");
+            animPlayerNode.Play(GameConstants.ANIM_MOVE);
         }
         // base._Input(@event);
     }
---
+++ Scripts/General/GameConstantcs.cs
@@ -0,0 +1,13 @@
+public class GameConstants
+{
+
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+
+    public const string MOVE_LEFT = "MoveLeft";
+    public const string MOVE_RIGHT = "MoveRight";
+    public const string MOVE_FORWARD = "MoveForward";
+    public const string MOVE_BACKWARD = "MoveBackward";
+
+
+}
