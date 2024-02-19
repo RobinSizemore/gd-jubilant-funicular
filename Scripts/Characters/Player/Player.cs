af7de224c374db702c3c19d13967300d8f2eb5fc
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-02-18 19:16:02 -0500

    Playing Animations and Exporting Fields with Attributes.

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -1,13 +1,33 @@
 using Godot;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public partial class Player : CharacterBody3D
 {
 
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer animPlayerNode;
+    [Export] private Sprite3D sprite3DNode;
+
+    public float Speed = 5.0f;
+
+    public override void _Ready()
+    {
+
+        animPlayerNode.Play("Idle");
+
+        /*
+        animPlayerNode = GetNode<AnimationPlayer>("AnimationPlayer");
+        base._Ready();
+        */
+        GD.Print(animPlayerNode.Name);
+        GD.Print(sprite3DNode.Name);
+    }
+
+
     private Vector2 direction = new Vector2();
     public override void _PhysicsProcess(double delta)
     {
-        int Speed = 5;
         Velocity = new Vector3(direction.X, 0, direction.Y).Normalized() * Speed;
 
         MoveAndSlide();
@@ -16,6 +36,14 @@ public partial class Player : CharacterBody3D
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        if (direction == Vector2.Zero)
+        {
+            animPlayerNode.Play("Idle");
+        }
+        else
+        {
+            animPlayerNode.Play("Move");
+        }
         // base._Input(@event);
     }
 }
