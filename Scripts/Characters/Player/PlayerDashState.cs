9bc2f3750ee372d121ed4ba288bbdbd1cefd17bb
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-02-21 19:07:35 -0500

    Cleaning up bugs.

--- Scripts/Characters/Player/PlayerDashState.cs
+++ Scripts/Characters/Player/PlayerDashState.cs
@@ -3,39 +3,24 @@ using System;
 
 public partial class PlayerDashState : Node
 {
-    Player characterNode;
-    [Export] Timer dashTimerNode;
+    private Player characterNode;
+    [Export] private Timer dashTimerNode;
+
     public override void _Ready()
     {
         characterNode = GetOwner<Player>();
-        SetPhysicsProcess(false);
         dashTimerNode.Timeout += HandleDashTimeout;
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-
-        if (characterNode.direction == Vector2.Zero)
-        {
-            characterNode.stateMachineNode.SwitchState<PlayerDashState>();
-        }
     }
 
     public override void _Notification(int what)
     {
-
         base._Notification(what);
-        if (what == GameConstants.NODE_ACTIVATE)
+
+        if (what == 5001)
         {
             characterNode.animPlayerNode.Play(GameConstants.ANIM_DASH);
-            SetPhysicsProcess(true);
             dashTimerNode.Start();
         }
-        else if (what == GameConstants.NODE_DEACTIVATE)
-        {
-            SetPhysicsProcess(false);
-        }
-
     }
 
     private void HandleDashTimeout()
